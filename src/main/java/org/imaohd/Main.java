d66cda2b1b994788e7c800c1e3b98fb7f191fa06
imaohd <ivanmao.hd@gmail.com>
2025-03-23 00:28:00 -0500

    refactor main

--- src/main/java/org/imaohd/Main.java
+++ src/main/java/org/imaohd/Main.java
@@ -12,21 +12,6 @@ public class Main {
         UserView userView = new UserView();
         UserController userController = new UserController(userDAO, userView);
 
-        // Script para crear la tabla en MySQL si no existe
-        System.out.println("Para crear la tabla en MySQL, ejecute el siguiente script:");
-        System.out.println("-----------------------------------------------------------");
-        System.out.println("CREATE DATABASE IF NOT EXISTS gestion_usuarios;");
-        System.out.println("USE gestion_usuarios;");
-        System.out.println("CREATE TABLE IF NOT EXISTS usuarios (");
-        System.out.println("  id INT AUTO_INCREMENT PRIMARY KEY,");
-        System.out.println("  nombre VARCHAR(50) NOT NULL,");
-        System.out.println("  apellido VARCHAR(50) NOT NULL,");
-        System.out.println("  email VARCHAR(100) NOT NULL,");
-        System.out.println("  telefono VARCHAR(20),");
-        System.out.println("  fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP");
-        System.out.println(");");
-        System.out.println("-----------------------------------------------------------");
-
         // Iniciar la aplicación
         userController.run();
     }
